9a8522b0108bb16fd63c026ef51d20a0964d7aef
rotemguy2002 <rotemguy2002@gmail.com>
2026-05-27 23:39:50 +0300

    started to add GetCurrDirCommand

--- Commands.cpp
+++ Commands.cpp
@@ -115,6 +115,9 @@ Command *SmallShell::CreateCommand(const char *cmd_line) {
     else if (firstWord.compare("showpid") == 0) {
         return new ShowPidCommand(cmd_line);
     }
+    else if (firstWord.compare("pwd") == 0) {
+        return new GetCurrDirCommand(cmd_line);
+    }
     else {
         return new ExternalCommand(cmd_line);
     }
@@ -147,5 +150,9 @@ void ChangePromptCommand::execute() {
 
 void ShowPidCommand::execute() {
     cout << getpid(); // need to change cout to the terminal
+}
+
+void GetCurrDirCommand::execute() {
+
 }
 
